a3a01b1d8f2a94372c5d4a742d160ce22d8ac9e2
James Coxhead <james.coxhead@capgemini.com>
2024-04-15 18:14:25 +0100

    351911: Team name in CreateFluxConfigRequest (#16)
    * Team name in CreateFluxConfigRequest
    
    * bump version
    
    * Update app setting in container app config

--- src/ADP.Portal.Api/Models/Flux/TeamFluxConfigRequest.cs
+++ src/ADP.Portal.Api/Models/Flux/TeamFluxConfigRequest.cs
@@ -4,6 +4,7 @@
     {
         public required string ProgrammeName { get; set; }
         public required string ServiceCode { get; set; }
+        public required string TeamName { get; set; }
         public required List<FluxService> Services { get; set; }
         public Dictionary<string, string> ConfigVariables { get; set; } = [];
     }
